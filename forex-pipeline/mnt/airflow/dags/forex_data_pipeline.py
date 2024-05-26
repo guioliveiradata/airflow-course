9274679550b2af34e2e9a1944344b86b7bd0d281
Guilherme de Oliveira <guilhermedtsc@gmail.com>
2024-05-26 09:19:18 -0300

    Implementing the FileSensor

--- forex-pipeline/mnt/airflow/dags/forex_data_pipeline.py
+++ forex-pipeline/mnt/airflow/dags/forex_data_pipeline.py
@@ -1,5 +1,6 @@
 from airflow import DAG
-from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.sensors.http import HttpSensor # type: ignore
+from airflow.sensors.filesystem import FileSensor  # type: ignore
 from datetime import datetime, timedelta
 
 default_args = {
@@ -17,7 +18,15 @@ with DAG('forex_data_pipeline', start_date=datetime(2024, 5, 1), schedule_interv
         task_id = 'is_forex_ratex_available',
         http_conn_id = 'forex_api',
         endpoint = 'marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b',
-        response_check = lambda response: 'rates' in response.txt,
+        response_check = lambda response: 'rates' in response.text,
+        poke_interval = 5,
+        timeout = 20
+    )
+
+    is_forex_currencies_file_available = FileSensor(
+        task_id = 'is_forex_currencies_file_available',
+        fs_conn_id = 'forex_path',
+        filepath = 'forex_currencies.csv',
         poke_interval = 5,
         timeout = 20
     )
